5f79f7172a2bf05b0bd1f18653aeab6d4f3d849b
Mariano Di Modugno <marianodimodugno@gmail.com>
2021-08-25 22:19:29 -0300

    Fix utils.sh & add full compose

--- devops/docker-compose-local/utils.sh
+++ devops/docker-compose-local/utils.sh
@@ -10,19 +10,19 @@ echo "Action: $ACTION"
 
 if [ "$ACTION" == "$DEPLOY" ]; then
    echo "Starting to deploy Docker-compose..."
-   docker-compose -f "$DOCKER_COMPOSE_FILE_NAME" up -d && docker-compose rm -fsv
+   docker-compose -f "$DOCKER_COMPOSE_FILE_NAME" up -d
    #docker-compose -f docker-compose-$parameterA.yml -p $parameterA up -d
    echo "Docker-compose is running!"
 
 elif [ "$ACTION" == "$UNDEPLOY" ]; then
    echo "Starting to undeploy Docker-compose..."
-   docker-compose -f "$DOCKER_COMPOSE_FILE_NAME" down -v
+   docker-compose -f "$DOCKER_COMPOSE_FILE_NAME" down -v && docker-compose rm -fsv
    #docker-compose -f docker-compose-$parameterA.yml -p $parameterA down
    echo "Docker-compose is down!"
 elif [ "$ACTION" == "$RESTART" ]; then
    echo "Starting to restart Docker-compose..."
-   docker-compose -f "$DOCKER_COMPOSE_FILE_NAME" down -v
-   docker-compose -f "$DOCKER_COMPOSE_FILE_NAME" up -d && docker-compose rm -fsv
+   docker-compose -f "$DOCKER_COMPOSE_FILE_NAME" down -v && docker-compose rm -fsv
+   docker-compose -f "$DOCKER_COMPOSE_FILE_NAME" up -d 
    #docker-compose -f docker-compose-$parameterA.yml -p $parameterA down
    echo "Docker-compose is restared!"
 else
